5a36c26f1f03ecf1f3ba05435c94ac08f7d18edd
Vamsi Atluri <vamc19@gmail.com>
2021-10-25 00:21:32 -0700

    Generate pages for each subsciption

--- staticfeed/static_feed.py
+++ staticfeed/static_feed.py
@@ -1,6 +1,6 @@
 import os.path
 from datetime import datetime, timezone
-from typing import Dict
+from typing import Dict, List
 
 import listparser
 from jinja2 import Environment, FileSystemLoader
@@ -45,26 +45,40 @@ class StaticFeed:
         self.entries.sort(key=lambda e: datetime.fromisoformat(e['updated_on']), reverse=True)
 
     def generate_html(self):
-        template = self._env.get_template('index.html')
-
         # Clean output dir. Assumes everything is a file for now.
         # This is dumb. Should Create a temp folder and replace output dir with it
         for path in os.listdir(self.output_dir):
             os.remove(os.path.join(self.output_dir, path))
 
+        # Generate html pages
+        footer = f"Last refreshed on {datetime.now(timezone.utc).strftime('%a %d %b, %Y at %H:%M %Z')}"
+
+        # Chronological feed - index.html
         page_title = "Latest Feed"
         page_subtitle = ""
-        footer = f"Last refreshed on {datetime.now(timezone.utc).strftime('%a %d %b, %Y at %H:%M %Z')}"
+        self._build_paginated_html('index.html', 'index', self.entries, page_title, page_subtitle, footer)
+
+        # Pages for each subscription
+        for subscription in self.subscriptions.values():
+            page_title = subscription.get_title()
+            page_subtitle = subscription.url
+            self._build_paginated_html('index.html', subscription.subscription_id, subscription.get_entries(),
+                                       page_title, page_subtitle, footer)
+
+    def _build_paginated_html(self, template_name: str, page_stub: str, entries: List,
+                              page_title: str, page_subtitle: str, footer: str):
+        template = self._env.get_template(template_name)
+
         index_start = 0
         page_num = 1
-        while index_start < len(self.entries):
+        while index_start < len(entries):
             index_end = index_start + self.entries_per_page
-            page_entries = self.entries[index_start:index_end]
-            next_page = f'{page_num + 1}.html' if index_end < len(self.entries) else None
+            page_entries = entries[index_start:index_end]
+            next_page = f'{page_stub}_{page_num + 1}.html' if index_end < len(entries) else None
 
             html = template.render(page_title=page_title, page_subtitle=page_subtitle,
                                    entries=page_entries, next_page=next_page, footer=footer)
-            file_name = 'index.html' if page_num == 1 else f'{page_num}.html'
+            file_name = f'{page_stub}.html' if page_num == 1 else f'{page_stub}_{page_num}.html'
             with open(os.path.join(self.output_dir, file_name), 'w') as page:
                 page.write(html)
 
